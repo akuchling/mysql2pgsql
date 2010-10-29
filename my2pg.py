8d36beb57c3625e1cec059a77d79f576c324e459
Andrew Kuchling <amk@amk.ca>
2010-10-29 14:39:49 -0400

    Decision: rework to use optparse, and not use SQLalchemy

--- my2pg.py
+++ my2pg.py
@@ -1,26 +1,56 @@
 #!/usr/bin/env python
 
 import sys
-#import argparse
-import sqlalchemy 
+import optparse
+import MySQLdb, psycopg2
 
 def main ():
-    mysql_host, mysql_db, pg_host, pg_db = sys.argv[1:]
-    
-    mysql_engine = create_engine('sqlite://')
-    pg_engine = create_engine('sqlite://')
+    parser = optparse.OptionParser(
+        '%prog [options] mysql-host mysql-db pg-host pg-db')
+    parser.add_option('--mysql-user',
+                      action="store",
+                      dest="mysql_user",
+                      help="User for login if not current user.")
+    parser.add_option('--mysql-password',
+                      action="store",
+                      dest="mysql_password",
+                      help="Password to use when connecting to server.")
+    parser.add_option('--pg-user',
+                      action="store",
+                      dest="pg_user",
+                      help="User for login if not current user.")
+    parser.add_option('--pg-password',
+                      action="store", default='',
+                      dest="pg_password",
+                      help="Password to use when connecting to server.")
 
-    mysql_meta = sqlalchemy.MetaData()
-    pg_meta = sqlalchemy.MetaData()
+    options, args = parser.parse_args()
+    if len(args) != 4:
+        parser.print_help()
+        sys.exit(1)
 
-    mysql_meta.reflect(bind=mysql_engine)
+    mysql_host, mysql_db, pg_host, pg_db = args
+
+    # Set up connections
+    mysql_conn = MySQLdb.Connection(
+        user=options.mysql_user,
+        passwd=options.mysql_password,
+        db=mysql_db,
+        host=mysql_host,
+        )
+    pg_conn = psycopg2.connect(
+        database=pg_db,
+        host=pg_host,
+        user=options.pg_user,
+        password=options.pg_password,
+        )
+
 
     # Convert tables
-    for t in mysql_meta.sorted_tables:
-        tbl = sqlalchemy.Table(t, mysql_meta, autoload=True, engine=mysql_engine)
-        tbl.create(pg_engine)
 
-        
+    # Close connections
+    mysql_conn.close()
+    pg_conn.close()
         
         
     
